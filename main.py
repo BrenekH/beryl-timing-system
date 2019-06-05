5b7fff5235aaac16eee9d5102047dd04bb420a96
MetaHerobrine1 <brenekharrison@gmai.com>
2019-06-04 23:55:33 -0600

    Disable checking for updates

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Main entry point
 if __name__ == "__main__":
+	import secrets
 	from robotics_game_timer import auto_updater
-	auto_updater.check_for_update()
+	# auto_updater.check_for_update(api_key=secrets.GITHUB_THINGS_TOKEN)
--- robotics_game_timer/auto_updater/__init__.py
+++ robotics_game_timer/auto_updater/__init__.py
@@ -1,7 +1,7 @@
 import os
 from .github_things import *
 
-def check_for_update():
+def check_for_update(api_key=os.getenv("GITHUB_THINGS_TOKEN")):
 	print("Checking for updates")
-	repo = Repository("zPaw", "kotlin-for-frc", os.getenv("GITHUB_THINGS_TOKEN"))
-	print("Kotlin For FRC is at version: " + repo.get_latest_tag()[1])
+	repo = Repository("zPaw", "robotics-camp-game-timer", api_key)
+	print("Robotics Camp Game Timer is at version: " + repo.get_latest_tag()[1])
