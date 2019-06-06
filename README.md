cf973852e2aa0defc6d9cd6241ee75e041afe35c
Cody Higgins <brenekharrison@gmail.com>
2019-06-06 09:13:31 -0600

    Update to not break gql_py

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Haywire Robotics Camp Game Timer
 A game timer for the Haywire Robotics summer camp.
+
+Note: Must be run on python interpreter 3.6.5 or higher
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Main entry point
 if __name__ == "__main__":
 	import secrets
-	# from robotics_game_timer import auto_updater
+	from robotics_game_timer import auto_updater
 	from robotics_game_timer import core
 	# auto_updater.check_for_update(api_key=secrets.GITHUB_THINGS_TOKEN)
 	display = core.CoreDisplay()
