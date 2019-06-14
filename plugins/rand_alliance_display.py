f8158cb00ee754e5c574787716f2b4293575880e
Cody Higgins <brenekharrison@gmail.com>
2019-06-14 09:16:36 -0600

    Working on rand_alliance_display

--- plugins/rand_alliance_display.py
+++ plugins/rand_alliance_display.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, socket, time, json
 from pathlib import Path
 
 class Plugin:
@@ -7,9 +7,22 @@ class Plugin:
 		self.requirements = ["display"]
 
 		self.bracket_id = "test"
+		self.cached_bracket = None
+		self.last_bracket_update = 0
+
+		TCP_IP, TCP_PORT = ("localhost", 1569)
+		self.bufsize = 16384
 
 		self.now_playing_image = pygame.image.load(str(Path("./plugins/Assets/images/NowPlaying.png")))
 		self.up_next_image = pygame.image.load(str(Path("./plugins/Assets/images/UpNext.png")))
+
+		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.s.connect((TCP_IP, TCP_PORT))
+
+		self.s.send("Hello".encode())
+		self.s.recv(self.bufsize)
+
+		self.s.send(f"chgbrkt{self.bracket_id}")
 
 	def register_listeners(self):
 		self.parent_class.register_on_loop_listener(self.on_loop)
@@ -22,9 +35,29 @@ class Plugin:
 		plugin_display.fill((255, 255, 255))
 		plugin_display_rect = plugin_display.get_rect()
 
-		# TODO: Place the team numbers on the image
+		self.get_bracket_from_server()
+
+		# TODO: Get the team numbers from the server
+		now_playing_red_team1, now_playing_red_team2 = ("99", "98")
+		now_playing_blue_team1, now_playing_blue_team2 = ("9", "8")
+		up_next_red_team1, up_next_red_team2 = ("97", "96")
+		up_next_blue_team1, up_next_blue_team2 = ("7", "6")
+
+		# Place numbers on the screen
 		now_playing_image_with_team = self.now_playing_image.copy()
 		up_next_image_with_team = self.up_next_image.copy()
+
+		self.message_to_surface(now_playing_image_with_team, now_playing_red_team1, (255, 0, 0), 100, 250, 450)
+		self.message_to_surface(now_playing_image_with_team, now_playing_red_team2, (255, 0, 0), 100, 250, 650)
+
+		self.message_to_surface(now_playing_image_with_team, now_playing_blue_team1, (0, 0, 255), 100, 750, 450)
+		self.message_to_surface(now_playing_image_with_team, now_playing_blue_team2, (0, 0, 255), 100, 750, 650)
+
+		self.message_to_surface(up_next_image_with_team, up_next_red_team1, (255, 0, 0), 100, 250, 450)
+		self.message_to_surface(up_next_image_with_team, up_next_red_team2, (255, 0, 0), 100, 250, 650)
+
+		self.message_to_surface(up_next_image_with_team, up_next_blue_team1, (0, 0, 255), 100, 750, 450)
+		self.message_to_surface(up_next_image_with_team, up_next_blue_team2, (0, 0, 255), 100, 750, 650)
 
 		# Scale the rects
 		target_side_length = int(plugin_display_rect.height * .75)
@@ -39,3 +72,31 @@ class Plugin:
 
 		plugin_display.blit(now_playing_transform, now_playing_rect)
 		plugin_display.blit(up_next_transform, up_next_rect)
+
+	def message_to_surface(self, surface, msg, color, size, x, y):
+		font = pygame.font.SysFont('Arial', size)
+		screen_text = font.render(msg, True, color)
+		screen_rect = screen_text.get_rect()
+		screen_rect.center = (x, y)
+		surface.blit(screen_text, screen_rect)
+
+	def message_to_surface_top_left(self, surface, msg, color, size, x, y):
+		font = pygame.font.SysFont('Arial', size)
+		screen_text = font.render(msg, True, color)
+		surface.blit(screen_text, (x, y))
+
+	def get_match_team_numbers_from_server(self):
+		# Return dict with nprt1, nprt2, npbt1, npbt2, uprt1, uprt2, upbt1, upbt2
+		return_dict = {}
+
+		return return_dict
+
+	def get_bracket_from_server(self):
+		if not self.last_bracket_update < int(time.time() - 1):
+			return self.cached_bracket
+		else:
+			self.s.send("gbifullbracket".encode())
+			self.cached_bracket = json.loads(self.s.recv(self.bufsize).decode('utf-8'))
+			print(self.cached_bracket)
+			return self.cached_bracket
+		
