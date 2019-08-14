bc3eff3cd877337b951ebf2172aa1a80d072fc3e
MetaHerobrine1 <brenekharrison@gmail.com>
2019-08-14 16:37:38 -0600

    Working on comparing versions

--- packages/beryl/beryl_timing_system/auto_updater/__init__.py
+++ packages/beryl/beryl_timing_system/auto_updater/__init__.py
@@ -1,5 +1,8 @@
 import os
+from semver import match
 from .github_things import *
 
 def check_for_update():
-	return False
+	# Return true if server version is greater than current version
+	# TODO: Read current installed version from ../__init__.py
+	return match("0.0.0", f'<{Repository("zPaw", "beryl-timing-system").get_latest_tag()}')
