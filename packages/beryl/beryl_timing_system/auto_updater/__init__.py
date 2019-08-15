fe235cd2bf0323386345a0debabb70fdc715bbad
Brenek Harrison <brenekharrison@gmail.com>
2019-08-14 19:14:20 -0600

    check_for_update looks at ../__init__.py for ver

--- packages/beryl/beryl_timing_system/auto_updater/__init__.py
+++ packages/beryl/beryl_timing_system/auto_updater/__init__.py
@@ -1,8 +1,8 @@
 import os
 from semver import match
 from .github_things import Repository
+from .. import version as software_version
 
 def check_for_update():
 	# Return true if server version is greater than current version
-	# TODO: Read current installed version from ../__init__.py
-	return match("0.0.0", f'<{Repository("zPaw", "beryl-timing-system").get_latest_tag()}')
+	return match(software_version, f'<{Repository("zPaw", "beryl-timing-system").get_latest_tag()}')
